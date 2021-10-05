f240d4285bcc77840b83de9a4f393c93a3634987
junh-park <jhpar25@hotmail.com>
2021-10-06 00:29:23 +1100

    Refactoring practice done using state pattern and segregation principles

---
+++ src/main/java/com/jun/refactoring/Customer.java
@@ -0,0 +1,67 @@
+package com.jun.refactoring;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Customer {
+	private String name;
+	private List<Rental> rentals = new ArrayList<Rental>();
+
+	public Customer(String name) {
+		this.name = name;
+	};
+
+	public void addRental(Rental rental) {
+		rentals.add(rental);
+	}
+
+	public String getName() {
+		return name;
+	};
+
+	public String statement() {
+		String result = "Rental Record for " + getName() + "\n";
+
+		Iterator<Rental> rentalIterator = rentals.iterator();
+		while (rentalIterator.hasNext()) {
+			Rental each = (Rental) rentalIterator.next();
+			// show figures for each rental
+			result += each.getMovie().getTitle() + ": " + String.valueOf(each.getCharge()) + "<BR>\n";
+		}
+
+		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+		result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
+		return result;
+	}
+
+	public String htmlStatement() {
+		String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+		Iterator<Rental> rentalIterator = rentals.iterator();
+		while (rentalIterator.hasNext()) {
+			Rental each = (Rental) rentalIterator.next();
+			// show figures for each rental
+			result += each.getMovie().getTitle() + ": " + String.valueOf(each.getCharge()) + "<BR>\n";
+		}
+		// add footer lines
+		result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+		result += "On this rental you earned <EM>" + String.valueOf(getTotalFrequentRenterPoints())
+				+ "</EM> frequent renter points<P>";
+		return result;
+	}
+
+	private double getTotalCharge() {
+		double result = 0;
+		for (Rental rental : rentals)
+			result += rental.getCharge();
+		return result;
+	}
+
+	private int getTotalFrequentRenterPoints() {
+		int result = 0;
+		for (Rental rental : rentals)
+			result += rental.getFrequentRenterPoints();
+		return result;
+	}
+
+}
---
+++ src/main/java/com/jun/refactoring/Rental.java
@@ -0,0 +1,27 @@
+package com.jun.refactoring;
+
+public class Rental {
+	private Movie movie;
+	private int daysRented;
+
+	public Rental(Movie movie, int daysRented) {
+		this.movie = movie;
+		this.daysRented = daysRented;
+	}
+
+	public int getDaysRented() {
+		return daysRented;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public double getCharge() {
+		return movie.price.getCharge(daysRented);
+	}
+	
+	public int getFrequentRenterPoints() {
+		return movie.getFrequentRenterPoints(daysRented);
+	}
+}
